9e7e2fc5f606c3700e99f34be1c4911209a19979
roldel <74282569+RolandDel@users.noreply.github.com>
2021-04-08 21:26:34 +0000

    ADD generic views/urls commented

--- polls/urls.py
+++ polls/urls.py
@@ -8,4 +8,14 @@ urlpatterns = [
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
+
 ]
+
+'''
+# GENERIC URLS
+
+    path('generic', views.IndexView.as_view(), name='index'),
+    path('generic/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('generic/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('generic/<int:question_id>/vote/', views.vote, name='vote'),
+'''
--- polls/views.py
+++ polls/views.py
@@ -34,3 +34,34 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+
+'''
+# GENERIC VIEWS
+
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+'''
